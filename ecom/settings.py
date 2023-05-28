f45901e2cb8802f3601d788e9f7b55afdf523e19
tasnimferdous <tasnimferdous216@gmail.com>
2023-05-28 17:15:11 +0600

    ...

--- ecom/settings.py
+++ ecom/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zngg2bi_5)=pty8e_jj&q16$y#-=jnllpg)1&0#g8tb#k*v!@k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
