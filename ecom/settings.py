aa5c6e39b13c55c8c23693e571c4e5bdb0b14247
tasnimferdous <tasnimferdous216@gmail.com>
2023-05-28 16:59:26 +0600

    ...d

--- ecom/settings.py
+++ ecom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zngg2bi_5)=pty8e_jj&q16$y#-=jnllpg)1&0#g8tb#k*v!@k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
