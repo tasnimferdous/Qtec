6d39896546aa841405d8171d5093c3518708f9cf
tasnimferdous <tasnimferdous216@gmail.com>
2023-05-28 16:23:41 +0600

    Vercel requirements updated

--- ecom/settings.py
+++ ecom/settings.py
@@ -80,15 +80,12 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'deterministic': False,
-        }
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
